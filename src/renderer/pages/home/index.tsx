1d9c110b08375950898d3de677b02b5fa766b901
徐焱挺 <xuyantiong@lidig.com>
2021-04-07 22:35:47 +0800

    select all

--- src/renderer/pages/home/index.tsx
+++ src/renderer/pages/home/index.tsx
@@ -1,21 +1,39 @@
-import type { FC } from 'react';
-// import React from 'react';
-import { Button } from 'antd';
+import { FC, useEffect, useState } from 'react';
+import { Button, Checkbox } from 'antd';
+import { useSelections } from 'ahooks';
 
 // import { routeTo } from '@/utils';
-// import { useDatabase  } from '@/hooks';
+import { useDatabase } from '@/hooks';
 
 import styles from './index.less';
 
 const Home: FC = () => {
-  // const { user } = useDatabase;
+  const { user } = useDatabase();
+  const [czzList, setCzz] = useState([]);
+  const [selections, setSelections] = useState<number[]>([]);
+  const {
+    // selected,
+    // allSelected,
+    isSelected,
+    toggle,
+    toggleAll,
+    // partiallySelected,
+  } = useSelections(selections, []);
+  useEffect(() => {
+    async function queryDB() {
+      const czzList = await user.finAll();
+      setCzz(czzList);
+      setSelections(czzList.map((el: { id: number }) => el.id));
+    }
+    queryDB();
+  }, []);
 
   return (
     <div className={styles.container}>
       <div className={styles.buttonGroup}>
         <Button
           onClick={() => {
-            console.log('select all');
+            toggleAll();
           }}
           type={'primary'}
         >
@@ -37,6 +55,20 @@ const Home: FC = () => {
         >
           下降
         </Button>
+      </div>
+      <div className={styles.czzList}>
+        {czzList.map((el: { id: number; name: string }, i) => {
+          return (
+            <div className={styles.infoCard}>
+              <Checkbox
+                checked={isSelected(el.id)}
+                onClick={() => toggle(el.id)}
+              >
+                {el.name}
+              </Checkbox>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
